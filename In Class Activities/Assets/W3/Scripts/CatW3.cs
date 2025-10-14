4b0a01685a63ff115423d5b078c109cb04645e26
tanhn2-uci <tanhn2@uci.edu>
2025-10-13 19:06:45 -0700

    Finish in-class activity w3

--- In Class Activities/Assets/W3/Scripts/CatW3.cs
+++ In Class Activities/Assets/W3/Scripts/CatW3.cs
@@ -96,7 +96,7 @@ public class CatW3 : MonoBehaviour
             // Notice this method's return type is void- that means we don't
             //      have to store the result anywhere.
             
-
+            DecreaseHealth();
             // STEP 2 ---------------------------------------------------------
 
             // STEP 6 ---------------------------------------------------------
@@ -107,7 +107,9 @@ public class CatW3 : MonoBehaviour
             //
             // Try toggling the Destroy Cat When Dead setting on the Inspector,
             //      and see how the cat is removed ONLY when it's checked!
-            
+            if(_health <= 0 && _destroyCatWhenDead){
+                DestroyCat();
+            } 
 
             // STEP 6 ---------------------------------------------------------
         }
@@ -129,13 +131,15 @@ public class CatW3 : MonoBehaviour
     private void DecreaseHealth()
     {
         // write Step 3 below this comment!
+        _health--;
+        _healthText.text = "health = " + _health;
 
 
         // STEP 5 -------------------------------------------------------------
         // Once you've finished Step 4, CALL the GetHealthSpeechText method
         //      and store the result in _speechText's text variable.
         // This will look very similar to the above line to change _healthText ;)
-
+        _speechText.text = GetHealthSpeechText();
 
         // STEP 5 -------------------------------------------------------------
     }
@@ -153,11 +157,16 @@ public class CatW3 : MonoBehaviour
     //      return "OH NO!".
     // 2. Otherwise, return "ouch".
 
-    //private ??? GetHealthSpeechText()
-    //{
+    private string GetHealthSpeechText()
+    {
+        if(_health < _maxHealth / 2){
+            return "OH NO!";
+        } else {
+            return "ouch";
+        }
         // put the method body here!
         
-    //}
+    }
     
     // STEP 4 -----------------------------------------------------------------
 
@@ -168,7 +177,7 @@ public class CatW3 : MonoBehaviour
         // Set the value of the _spriteRenderer's color variable to the value
         //      of the ball's ballRenderer's color variable.
         // This means you'll need to use the '.' twice to get to the color :)
-
+        _spriteRenderer.color = ball.ballRenderer.color;
 
         // STEP 7 -------------------------------------------------------------
     }
--- README.md
+++ README.md
@@ -9,6 +9,11 @@ Attached player component into a cat object to give it ability to move with AWSD
 2.The _bounces variable have to be an int type because we represent a total number of bounces as a whole number. It would make no sense if the ball bounces and we add up 0.8 or 0.6 so we can't use float type in this circumstance. Boolean is only can hold true or false so this is also not a good choice. String type is the one we can use to display whole number as a text but we can not increment or adding up whenever the ball bounces.
 
 3.The error stated "Literal of type double cannot be implicitly converted to type float. Use an F suffix to create a literal of this type"
+
+### W3
+Table #12: Our method would take two parameters. The first parameter. The first parameter would be int type would take how closed is this relationship based on number and the second parameter is boolean to detemrine whether or not character know secret. And the return type for this method will be string type which is a text response. 
+
+Think of a class as a blueprint for a robot in a robot factory. The blueprint defines what parts the robot has and what tasks it can perform, but it isn’t an actual robot yet. When you use that blueprint to build a real robot, that robot becomes a Component. The member variables are the robot’s traits or features like its color, size, or battery capacity These can differ from one robot to another, even if they come from the same blueprint. The methods are the robot’s abilities or actions, such as walking, talking, or picking up objects. These are the things every robot built from that blueprint knows how to do.
 ## Open-Source Assets
 ### W1
 - Animals: https://assetstore.unity.com/packages/3d/characters/animals/animals-free-animated-low-poly-3d-models-260727 
